e0317214d6faadaab88e3620585bbf29f605e307
wit <witwitchayakarn@gmail.com>
2023-11-16 14:39:27 +0700

    Use variable fonts if support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: [
     './*.html',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
