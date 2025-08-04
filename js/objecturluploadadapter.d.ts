5b590dbd109835da1796d885a38a817eb26f5dcd
wit <witwitchayakarn@gmail.com>
2025-08-04 10:59:08 +0700

    Upgrade CKEditor

--- js/objecturluploadadapter.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ObjectUrlUploadAdapter extends Plugin {
-    static get requires(): (typeof FileRepository)[];
-    static get pluginName(): string;
-    init(): void;
-}
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
